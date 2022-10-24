de2496d8dcec5f67e073c5d9fc1536aba1829a4a
Kristina Holsapple <54590825+krishols@users.noreply.github.com>
2022-10-24 19:16:19 -0400

    last shot lol

--- server/index.js
+++ server/index.js
@@ -60,6 +60,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 //use cors to allow cross origin resource sharing
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
 
 app.use(cors());
 //app.options('*', cors());
